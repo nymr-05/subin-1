9f46c4c0dfbfbb78dff3a322a81cb49f7d015f47
Naimur Rahman <84228967+nymr-05@users.noreply.github.com>
2021-10-29 09:40:26 +0600

    Create 3.6.cpp

---
+++ 3.6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+  int n = 10;
+  if (n < 30){
+    cout << "n is less than 30." << endl;
+  }else if(n < 50){
+    cout << "n is less than 50." << endl;
+  }
+  return 0;
+}
+  
