54654379ae0a6b380dad586f1c9c72151744fdd1
Naimur Rahman <84228967+nymr-05@users.noreply.github.com>
2021-12-03 15:30:12 +0600

    Create 4.5.cpp

---
+++ 4.5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    int i = 1;
+
+    while(i<=10){
+        cout << n <<" X " << i << " = " << n*i << endl;
+        i++;
+    }
+    return 0;
+}
